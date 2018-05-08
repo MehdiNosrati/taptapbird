3adecc003044d7a6e8080227cdda5b744f67b6ad
mehdinosrati <mehdins@aut.ac.ir>
2018-05-09 00:25:07 +0430

    o_o

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-var x = 0;
+
 var l = 400;
+var ol = 2000;
+
+var obs;
+var i;
+var oo = 0;
+
+
 var interv = setInterval(function(){
-		l += 10;
+		l += 1;
 		document.getElementById("b").style.left = l+'px' ;
 		window.scrollTo(l, 0);
-	},20);
+		
+		
+	
+
+		
+	
+	
+	},1);
 function funcJump() {
+	document.getElementById("b").style.transform = "rotate(-35deg)";
 	document.getElementById("b").style.top = "100px";
 	document.getElementById("b").style.transition = "top .3s";
 	
@@ -13,21 +28,48 @@ function funcJump() {
 		
 		document.getElementById("b").style.top = "320px";
 		document.getElementById("b").style.transition = "top .3s";
+		document.getElementById("b").style.transform = "rotate(35deg)";
+		setTimeout(function(){
+			document.getElementById("b").style.transform = "rotate(0deg)";
+		},10);
 		
-	}, 200);
+	}, 300);
 	
 }
-function funcT(event){
-	if (event.altKey){
-		clearInterval(interv);
-		}
-	else if (event.ctrlKey){
+function funcDJump(){
+	document.getElementById("b").style.transform = "rotate(-35deg)";
+	document.getElementById("b").style.top = "150px";
+	document.getElementById("b").style.transition = "top .3s";
+	
+	setTimeout(function(){
 		
-		interv = setInterval(function(){
-		l += 10;
-		document.getElementById("b").style.left = l+'px' ;
-		window.scrollTo(l, 0);
-	},20);
-	}
-	else alert("kk")
+		document.getElementById("b").style.top = "50px";
+		document.getElementById("b").style.transition = "top .3s";
+		document.getElementById("b").style.transform = "rotate(-35deg)"
+		setTimeout(function(){
+			document.getElementById("b").style.top = "320px";
+		document.getElementById("b").style.transition = "top .3s";
+		document.getElementById("b").style.transform = "rotate(0deg)"
+		}, 300);
+		
+	}, 300);
+	
 }
+
+
+setInterval(function(){
+	var ob = document.createElement("div");
+	ob.className = "obstacle";
+	
+	ol += Math.floor((Math.random() * 1000 + 500));
+	ob.style.position = "relative";
+	ob.style.width = "100px";
+	ob.style.height = "250px";
+	ob.style.left = ol+'px';
+	ob.style.top = "200px";
+	ob.style.display = "inline-block"; 
+	ob.style.backgroundColor = "brown";
+	ob.style.borderRadius = "40px 40px 0px 0px";
+	document.getElementById("addo").appendChild(ob);
+},1350);
+
